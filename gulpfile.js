8cf5eeb6136313979a5baef10b59c2b222aa3715
Vincent Séguin <vincent.seguin.1@ulaval.ca>
2015-05-03 17:03:11 -0400

    + Add gulpfile and cleanup

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var runSequence = require('run-sequence');
+var spawn = require('child_process').spawn;
+var symlink = require('gulp-symlink');
+var node;
+
+var paths = {
+    server: ['index.js', './models/**/*.js', './routes/**/*.js']
+};
+
+gulp.task('server', function () {
+    if (node) {
+        node.kill();
+    }
+    node = spawn('node', paths.server, {stdio: 'inherit'});
+    node.on('close', function (code) {
+        if (code === 8) {
+            gutil.log('Error detected, waiting for changes...');
+        }
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.server, ['server']);
+});
+
+gulp.task('hook', function () {
+    return gulp.src('.pre-commit')
+        .pipe(symlink('.git/hooks/', 'pre-commit'));
+});
+
+gulp.task('default', function () {
+    runSequence('server', 'watch');
+});
+
+process.on('exit', function () {
+    if (node) {
+        node.kill();
+    }
+});
